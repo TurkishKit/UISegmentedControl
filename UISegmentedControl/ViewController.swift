b0458db33383bf9da9ae75e202f6a7466dd45ccf
Can Balkaya <canbalkya@gmail.com>
2020-06-21 18:00:50 +0300

    Project is added.

---
+++ UISegmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by Can Balkaya on 6/21/20.
+//  Copyright © 2020 Can Balkaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+    }
+}
